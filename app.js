8013af6bb09b6cc962ec86f15ba6326284287387
Ghaida Ja'aysah <ghaida.20004@gmail.com>
2025-11-10 09:35:38 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict"; // Enable strict mode for better error checking ECMAScript 5 feature
+// Define moods
+const moods = [
+    {text: 'Happy', emoji: '😊', color: 'green'},
+    {text: 'Sad', emoji: '😢', color: 'blue'},
+    {text: 'Angry', emoji: '😠', color: 'red'},
+    {text: 'Calm', emoji: '😌', color: 'lightblue'},
+    {text: 'Focused', emoji: '🧠', color: 'lightblue'},
+    {text: 'Chill', emoji: '😎', color: 'lightgreen'}
+];
+
+
+
+
+ 
